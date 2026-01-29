423643ba2323a471ffb90d978c558dcedabbb144
Huzaifafaheem <faheemhuzaifa93@gmail.com>
2026-01-30 00:52:26 +0500

    Fix deployment: Add fallback Sanity project ID and dataset

--- sanity/env.ts
+++ sanity/env.ts
@@ -1,7 +1,7 @@
 export const apiVersion =
   process.env.NEXT_PUBLIC_SANITY_API_VERSION || '2025-01-22'
 
-export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET;
-export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID;
+export const dataset = process.env.NEXT_PUBLIC_SANITY_DATASET || 'production';
+export const projectId = process.env.NEXT_PUBLIC_SANITY_PROJECT_ID || 'orfgvhfr';
 
 export const useCdn = false
